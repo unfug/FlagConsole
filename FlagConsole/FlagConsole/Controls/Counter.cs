d4c8a4ec5dd1bbd3509279a5ef9773c7a879996c
cabua <marius.sharma@gmail.com>
2012-12-23 17:51:33 +0100

    Added two new controls. Controls/Counter.cs  =  Use Up and Down Key to set int value Controls/Switch.cs = Use Spacebar to switch between True and False

---
+++ FlagConsole/FlagConsole/Controls/Counter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlagConsole.Controls;
+using FlagConsole.Drawing;
+
+namespace FlagConsole.Controls
+{
+    public class Counter : Control, IFocusable
+    {
+        public int _value;
+        public int Value {
+            get { return this._value; } 
+            set
+            {
+                if (value < 0) { _value = 0; return; }
+                _value = value;
+            }
+        }
+        protected override void Draw(GraphicBuffer buffer)
+        {
+            
+            buffer.BackgroundDrawingColor = this.BackgroundColor;
+            buffer.ForegroundDrawingColor = this.ForegroundColor;
+
+            string background = String.Empty;
+            background = background.PadRight(this.Size.Width, ' ');
+
+            buffer.DrawLine(background, Coordinate.Origin);
+
+            buffer.DrawLine(this.Value.ToString(), Coordinate.Origin);
+        }
+
+        public Counter()
+        {
+            this.BackgroundColor = ConsoleColor.White;
+            this.ForegroundColor = ConsoleColor.Black;
+            this.Value = 1;
+        }
+
+        public void Defocus()
+        {
+            this.IsFocused = false;
+            this.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void Focus()
+        {
+            this.IsFocused = true;
+            this.IsVisible = true;
+            this.ForegroundColor = ConsoleColor.DarkRed;
+            this.ScanInput();
+        }
+
+        private void ScanInput()
+
+        {
+            bool cursorVisible = Console.CursorVisible;
+            ConsoleKeyInfo key;
+           
+
+            //Console.CursorVisible = true;
+
+            do
+            {
+                this.Invalidate();
+                 key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    this.Value++;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    this.Value--;
+                }
+            }
+            while (key.Key != ConsoleKey.Q && this.IsVisible);
+            //Console.CursorVisible = cursorVisible;
+
+            this.Defocus();
+        }
+
+       
+
+        public char[] Text { get; set; }
+
+        public bool IsFocused { get; private set; }
+    }
+}
---
+++ FlagConsole/FlagConsole/Controls/Switch.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlagConsole.Controls;
+using FlagConsole.Drawing;
+
+namespace FlagConsole.Controls
+{
+    public class Switch : Control, IFocusable
+    {
+        
+        public bool _value;
+        public bool Value { 
+            get{return _value;}
+            set { _value = value; }
+        }
+
+        public event EventHandler FocusChanged;
+        protected override void Draw(GraphicBuffer buffer)
+        {
+            
+            buffer.BackgroundDrawingColor = this.BackgroundColor;
+            buffer.ForegroundDrawingColor = this.ForegroundColor;
+
+            string background = String.Empty;
+            background = background.PadRight(this.Size.Width, ' ');
+
+            buffer.DrawLine(background, Coordinate.Origin);
+
+            buffer.DrawLine(this.Value.ToString(), Coordinate.Origin);
+        }
+
+        public Switch()
+        {
+            this.BackgroundColor = ConsoleColor.White;
+            this.ForegroundColor = ConsoleColor.Black;
+            this.Value = false;
+        }
+
+        public void Defocus()
+        {
+            this.IsFocused = false;
+            this.ForegroundColor = ConsoleColor.Black;
+            OnFocusChanged(EventArgs.Empty);
+        
+        }
+
+        public void Focus()
+        {
+            this.IsFocused = true;
+            this.ForegroundColor = ConsoleColor.DarkRed;
+            this.IsVisible = true;
+            this.ScanInput();
+        }
+
+        private void ScanInput()
+
+        {
+            bool cursorVisible = Console.CursorVisible;
+            ConsoleKeyInfo key;
+           
+
+            //Console.CursorVisible = true;
+
+            do
+            {
+                this.Invalidate();
+                 key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Spacebar)
+                {
+                    this.Value = !Value;
+                }
+            }
+            while (key.Key != ConsoleKey.Q && this.IsVisible);
+            
+
+            this.Defocus();
+        }
+
+       
+
+        public char[] Text { get; set; }
+
+        public bool IsFocused { get; private set; }
+
+        public virtual void OnFocusChanged(EventArgs e)
+        {
+            if (this.FocusChanged != null)
+            {
+                this.FocusChanged(this, e);
+            }
+        }
+    }
+}
